536e80caca4b379390dd9d4cadadf3b393c60a0c
Subhadip023 <subhadip240420@gmail.com>
2024-02-04 23:58:53 +0530

    adjsut nav

--- projects/sfi-webside/index.js
+++ projects/sfi-webside/index.js
@@ -1,6 +1,5 @@
 const dropdrowen=document.querySelector('.dropdowen');
 const navbar=document.querySelector('.navbar');
-console.log(navbar)
 dropdrowen.addEventListener('click',function(){
     navbar.style.display = navbar.style.display === "none" ? "block" : "none";
 
