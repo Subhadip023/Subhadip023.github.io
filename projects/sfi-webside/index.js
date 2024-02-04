5e241fed54e89af03d61793d4dd9f172f08768d8
Subhadip023 <subhadip240420@gmail.com>
2024-02-05 00:12:38 +0530

    config nav

--- projects/sfi-webside/index.js
+++ projects/sfi-webside/index.js
@@ -1,6 +1,15 @@
 const dropdrowen=document.querySelector('.dropdowen');
 const navbar=document.querySelector('.navbar');
+const links=document.querySelector('.navbar ul li');
 dropdrowen.addEventListener('click',function(){
     navbar.style.display = navbar.style.display === "none" ? "block" : "none";
+    
+});
+
+links.forEach(function(link){
+link.addEventListener("click",function(){
+    navbar.style.display = navbar.style.display === "none" ? "block" : "none";
+
+});
 
 });
