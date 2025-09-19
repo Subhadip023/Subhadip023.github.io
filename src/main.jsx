e41de26ec9d6f7e52970c5fe809e838d142101b0
Anuragprasad427 <162587953+Anuragprasad427@users.noreply.github.com>
2025-09-19 17:05:49 +0530

    fix: update script source in index.html and switch to createBrowserRouter in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createHashRouter, RouterProvider } from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import About from '../pages/About.jsx'
 import Home from '../pages/Home.jsx'
 import './index.css'
 import App from './App.jsx'
 
-const router = createHashRouter([
+const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [
-      { path: "/about", element: <About /> },
-      { path: "/home", element: <Home /> },
+      { path: "about", element: <About /> },
+      { path: "home", element: <Home /> },
     ]
   },
 ]);
